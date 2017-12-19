17cd43f46cc3cd3f3b61c6b55d546a0bf8495672
Diallo Ibrahima <ibrahimasory22@hotmail.com>
2017-12-19 11:09:29 +0000

    added some coments

--- accmes3.cpp
+++ accmes3.cpp
@@ -6,7 +6,7 @@
 #include<string>
 #include<iomanip>
 using namespace std;
-// added a coment 
+// added a coment my name is ibrahima sory diallo
 const int CAPACITY = 100;
 struct Vector_enter {
 	unsigned T[CAPACITY];
